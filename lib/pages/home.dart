282ea21a6fac674ddca6319360c56b38a3a4a05a
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2025-04-12 22:27:41 -0300

    primeiros passos

---
+++ lib/pages/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text("Text"),
+            Text("Text"),
+            Text("Text"),
+            Text("Text"),
+          ],
+        ),
+      ),
+    );
+  }
+}
