5597b9202c361e2dd28ca0fe6d460fe4275b1bf4
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2025-04-14 22:42:14 -0300

    Finalizando projeto

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,12 +12,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List options = ["pedra", "papel", "tesoura"];
+
+  String _message = "Sua Jogada: ";
+  String _imagePath = "images/default.png";
+
+  void _play(String choice) {
+    int i = Random().nextInt(options.length);
+
+    String randomChoice = options[i];
+    this._imagePath = "images/" + randomChoice + ".png";
+    print(
+      "Jogada do jogador: " + choice + ", Jogada da máquina: " + randomChoice,
+    );
+    if ((choice == "pedra" && randomChoice == "tesoura") ||
+        (choice == "papel" && randomChoice == "pedra") ||
+        (choice == "tesoura" && randomChoice == "papel")) {
+      // Jogador venceu
+      setState(() {
+        this._message = "Voce venceu!";
+      });
+    } else if ((choice == "pedra" && randomChoice == "papel") ||
+        (choice == "papel" && randomChoice == "tesoura") ||
+        (choice == "tesoura" && randomChoice == "pedra")) {
+      // Jogador Perdeu
+      setState(() {
+        this._message = "Voce Perdeu!";
+      });
+    } else {
+      //Empate
+      setState(() {
+        this._message = "Empatou!";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(10, 0, 55, 1),
       appBar: AppBar(title: Text("JokenPo")),
       body: Container(
+        padding: EdgeInsets.only(bottom: 150),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Padding(
               padding: EdgeInsets.only(top: 32, bottom: 16),
@@ -23,28 +63,35 @@ class _HomePageState extends State<HomePage> {
                 "Jogada da Máquina!",
                 style: TextStyle(
                   fontSize: 20,
-                  fontWeight: FontWeight.bold
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
             ),
-            Image.asset("images/default.png", height: 150),
+            Image.asset(_imagePath, height: 150),
             Padding(
               padding: EdgeInsets.only(top: 32, bottom: 16),
               child: Text(
-                "Sua Jogada!",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
+                _message,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset("images/pedra.png", height: 100),
-                Image.asset("images/papel.png", height: 100),
-                Image.asset("images/tesoura.png", height: 100),
+                GestureDetector(
+                  onTap: () => _play("pedra"),
+                  child: Image.asset("images/pedra.png", height: 120),
+                ),
+                GestureDetector(
+                  onTap: () => _play("papel"),
+                  child: Image.asset("images/papel.png", height: 120),
+                ),
+                GestureDetector(
+                  onTap: () => _play("tesoura"),
+                  child: Image.asset("images/tesoura.png", height: 120),
+                ),
               ],
             ),
           ],
