2f93874f8c03d5b221a9badd713900d2be8443e2
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2025-04-16 18:44:20 -0300

    changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,36 +11,41 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List options = ["pedra", "papel", "tesoura"];
+  var points = 0;
+
+  static const primaryColor = Color.fromARGB(255, 255, 255, 255);
 
   String _message = "Sua Jogada: ";
   String _imagePath = "images/default.png";
+  var imageApp = AssetImage("images/default.png");
 
   void _play(String choice) {
     int i = Random().nextInt(options.length);
 
     String randomChoice = options[i];
-    this._imagePath = "images/" + randomChoice + ".png";
-    print(
-      "Jogada do jogador: " + choice + ", Jogada da máquina: " + randomChoice,
-    );
+    //_imagePath = "images/$randomChoice.png";
+    this.imageApp = AssetImage("images/$randomChoice.png");
+    print("Jogada do jogador: $choice, Jogada da máquina: $randomChoice");
     if ((choice == "pedra" && randomChoice == "tesoura") ||
         (choice == "papel" && randomChoice == "pedra") ||
         (choice == "tesoura" && randomChoice == "papel")) {
       // Jogador venceu
       setState(() {
-        this._message = "Voce venceu!";
+        _message = "Voce venceu!";
+        points++;
       });
     } else if ((choice == "pedra" && randomChoice == "papel") ||
         (choice == "papel" && randomChoice == "tesoura") ||
         (choice == "tesoura" && randomChoice == "pedra")) {
       // Jogador Perdeu
       setState(() {
-        this._message = "Voce Perdeu!";
+        _message = "Voce Perdeu!";
+        points--;
       });
     } else {
       //Empate
       setState(() {
-        this._message = "Empatou!";
+        _message = "Empatou!";
       });
     }
   }
@@ -51,7 +54,12 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(10, 0, 55, 1),
-      appBar: AppBar(title: Text("JokenPo")),
+      appBar: AppBar(
+        title: Center(
+          child: Text("JokenPo", style: TextStyle(color: Colors.white)),
+        ),
+        backgroundColor: Color.fromRGBO(10, 0, 55, 1),
+      ),
       body: Container(
         padding: EdgeInsets.only(bottom: 150),
         child: Column(
@@ -68,12 +76,29 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Image.asset(_imagePath, height: 150),
+            Image(image: imageApp, height: 150),
             Padding(
               padding: EdgeInsets.only(top: 32, bottom: 16),
+              //Jogada do Jogador
               child: Text(
                 _message,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 10),
+              //Jogada do Jogador
+              child: Text(
+                points.toString(),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
             Row(
